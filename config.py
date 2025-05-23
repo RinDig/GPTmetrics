660d95826c29f93812c5166bf294c7053204c56b
J.E. Van Clief <vancliefmedia@gmail.com>
2025-05-23 16:13:39 +0100

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-# Configuration for CCR analysis module (can be expanded)
-# Currently, most CCR specific configs are in the main app_config.py
-# This file is a placeholder for more specific CCR settings if needed.
-
-# Example: if ccr_wrapper had more specific tunable parameters not suitable for global app_config
-# CCR_SIMILARITY_THRESHOLD = 0.5 
-# CCR_EMBEDDING_MODEL_VARIANT = 'paraphrase-MiniLM-L3-v2' # If different from main SBERT model 
